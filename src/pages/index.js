ffb6183af3bdbc9551ca9d292a419e15964cf255
tim901231 <b09901020@ntu.edu.tw>
2021-10-04 12:47:08 +0800

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Layout from "../components/layout";
-
+import { StaticImage } from "gatsby-plugin-image";
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
