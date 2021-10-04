e3a4da251d3a815041491058454ba7d48fed0c69
tim901231 <b09901020@ntu.edu.tw>
2021-10-04 13:48:18 +0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "my-second-website",
+    title: "my-second-website2",
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
